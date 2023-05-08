05ad3c4faa5ffd4968606d933102474ee2f376d9
Eyal Arkush <eyal@arkush.co.il>
2023-05-08 13:05:04 +0300

    updated

--- Sign In.js
+++ Sign In.js
@@ -26,24 +26,29 @@
 // // Attach event listener to login form submit button
 // document.getElementById("loginBtn").addEventListener("click", handleLogin);
 
-console.log("bbbbbb")
-function handleLogin(event) {
-    event.preventDefault(); // Prevent default form submission
 
-    // Get email and password input values
-    const email = document.getElementById("accountEmail").value;
-    const password = document.getElementById("psw").value;
 
-    // Check if email is authorized
-    if (authorizedUsers.includes(email)) {
-        // Do further validation, such as checking the password, and redirect to the user's dashboard
-        console.log("Login successful!");
-        window.location.href = "../home/homePage.html";
-    } else {
-        // Show error message if email is not authorized
-        console.log("Login failed! Unauthorized user.");
-        alert("You are not authorized to access this website.");
-    }
+//זה לא היה בהערה זה לעמית!!!!!!!!!!!
 
-    return false; // Added line to prevent default form submission
-}
+
+// console.log("bbbbbb")
+// function handleLogin(event) {
+//     event.preventDefault(); // Prevent default form submission
+//
+//     // Get email and password input values
+//     const email = document.getElementById("accountEmail").value;
+//     const password = document.getElementById("psw").value;
+//
+//     // Check if email is authorized
+//     if (authorizedUsers.includes(email)) {
+//         // Do further validation, such as checking the password, and redirect to the user's dashboard
+//         console.log("Login successful!");
+//         window.location.href = "../home/homePage.html";
+//     } else {
+//         // Show error message if email is not authorized
+//         console.log("Login failed! Unauthorized user.");
+//         alert("You are not authorized to access this website.");
+//     }
+//
+//     return false; // Added line to prevent default form submission
+// }
