e802de57707894252a14d82c7f8f334e423b68f0
Eyal Arkush <eyal@arkush.co.il>
2023-05-09 17:55:59 +0300

    updated

--- items/products.js
+++ items/products.js
@@ -1,55 +1,23 @@
-// const products = [
-//     // {
-//     //     ID: 1
-//     //     name: 'Air Jordan 1 Hyper Royal',
-//     //     description: 'This is the description of product 1',
-//     //     price: 99.99,
-//     // },
-//
-//     {
-//         ID: 2
-//         name: 'Yeezy Boost 350',
-//         description: 'This is the description of product 2',
-//         price: 149.99,
-//     },
-//     // Add more products as needed
-// ];
-//
-//         function displayProducts() {
-//         const container = document.querySelector('.product-container');
-//
-//         let html = '';
-//         products.forEach(product => {
-//         html += `
-//       <div class="product">
-//         <img src="${product.image}" alt="${product.name}">
-//         <h3>${product.name}</h3>
-//         <p>${product.description}</p>
-//         <span>${product.price}</span>
-//         <button>Add to cart</button>
-//       </div>
-//     `;
-//     });
-//
-//         container.innerHTML = html;
-//
-//         displayProducts();
 
-fetch("products.json").then(function (response){
-        return response.json();
-}).then(function (products){
+fetch("products.json").then(function (response) {
+    return response.json();
+}).then(function (products) {
     console.log(products);
     let placeholder = document.querySelector("#data-output");
-    let out="";
-    for(let product of products){
-        out +=`
-        <tr>
-<!--            <td> <img src='${product.image}'> </td>-->
-            <td> <img src='${product.image}'> </td>
-            <td> ${product.name} </td>
-            <td> ${product.price} </td>
-    
-        `;
+    let out = "";
+    for (let product of products) {
+        out += `
+      <tr>
+        <td>${product.name}</td>
+        <td>${product.price}</td>
+        <td>
+          <img src="${product.image}" 
+               data-hover="${product.image2}" 
+               onmouseover="this.src='${product.image2}'" 
+               onmouseout="this.src='${product.image}'">
+        </td>
+      </tr>
+    `;
     }
-placeholder.innerHTML = out;
+    placeholder.innerHTML = out;
 })
