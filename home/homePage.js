8a875641fbf1a303733e37d3d8ca97a6b12783f7
Eyal Arkush <eyal@arkush.co.il>
2023-05-10 17:41:35 +0300

    updated

--- home/homePage.js
+++ home/homePage.js
@@ -12,70 +12,47 @@
 
 });
 
+    document.addEventListener("DOMContentLoaded", function() {
+        showSlides();
+        hidePhoto();
+    });
 
+    var slideIndex = 0;
 
-document.addEventListener("DOMContentLoaded", function() {
-    showSlides()});
+    function showSlides() {
+        var slides = document.getElementsByClassName("mySlides");
+        if (slides.length === 0) {
+            // No slides found with the class "mySlides"
+            return;
+        }
 
-document.addEventListener('DOMContentLoaded', function() {
+        for (var i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+
+        slideIndex++;
+        if (slideIndex > slides.length) {
+            slideIndex = 1;
+        }
+
+        slides[slideIndex - 1].style.display = "block";
+        setTimeout(showSlides, 3000);
+    }
+
+    showSlides();
+
+    function hidePhoto() {
+        let date = new Date();
+        let hour = date.getHours();
+        let sun = document.querySelector("#sun") ?? document.createElement('img');
+        let moon = document.querySelector("#moon") ?? document.createElement('img');
+
+        if (hour > 19 && hour < 6) {
+            clockImg.src = "../pics/clockPics/moon.png"
+        }
+        else {
+            clockImg.src = "../pics/clockPics/sun.png"
+        }
+    }
+
     hidePhoto();
-});
-
-
-
-
-function currentTime() {
-    let date = new Date();
-    let hh = date.getHours();
-    let session = "AM";
-
-    if(hh > 12){
-        session = "PM";
-    }
-    console.log('The time is', session);
-
-    return session;
-}
-
-function hidePhoto() {
-    console.log('hidePhoto called');
-    let sun = document.querySelector("#sun") ?? document.createElement('img');
-    let moon = document.querySelector("#moon") ?? document.createElement('img');
-
-
-    console.log('sun is', sun);
-    console.log('moon is', moon);
-
-
-    let time = currentTime();
-    console.log('time is', time);
-
-    if (time === 'AM') {
-        sun.style.visibility = 'visible';
-        moon.style.visibility = 'hidden';
-    } else {
-        sun.style.visibility = 'hidden';
-        moon.style.visibility = 'visible';
-    }
-}
-
-hidePhoto();
-
-
-
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {
-        slideIndex = 1;
-    }
-    slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 3000); // Change image every 2 seconds
-}
