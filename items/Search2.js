7bc049b665589a556c69e74c03b46d78c93395f5
amitf <fedaeli@post.bgu.ac.il>
2023-05-16 15:48:32 +0300

    ./

--- items/Search2.js
+++ items/Search2.js
@@ -1,10 +1,14 @@
+console.log("check1222")
+
 function search2() {
     document.getElementById("searchForm").addEventListener("submit", function(event) {
+        console.log("check0")
+
         event.preventDefault(); // Prevent form submission
 
         const searchInput = document.getElementById("search").value.toLowerCase();
         console.log("Search input:", searchInput);
-
+        console.log("check1")
         // Use the searchInput variable
 
         fetch("products.json") /* connect to the products json page*/
@@ -14,6 +18,7 @@ function search2() {
             .then(function (products) {
                 let placeholder = document.querySelector("#data-output");
                 let out = "";
+                console.log("check2")
                 for (let product of products) {  /* check if there is a product with a name like the search input*/
                     const productName = product.name.toLowerCase(); //new
                     if (productName.includes(searchInput)){
@@ -42,3 +47,5 @@ function search2() {
 
     });
 }
+
+search2();
