cbe6ef79c2fe4cdbe908ab68011371a3cf88fdb5
amitf <fedaeli@post.bgu.ac.il>
2023-06-12 22:12:49 +0300

    after server

--- express_server/app.js
+++ express_server/app.js
@@ -2,15 +2,16 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = 3000;
+const crud = require("../express_server/static/crud");
+const sql = require('../express_server/db/db');
 const BodyParser = require("body-parser");
-app.use(BodyParser.json());
 const cookieParser = require("cookie-parser");
+app.use(BodyParser.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname,"static")));
 app.use(BodyParser.urlencoded({extended:true}));
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine','pug');
-const sql = require('../express_server/db/db');
 
 app.get('/',(req,res) => {
      res.sendFile(path.join(__dirname,"views/signIn.html"));
@@ -25,8 +26,6 @@ app.get('/contactUs',(req,res) => {
 app.get('/homePage',(req,res) => {
     res.sendFile(path.join(__dirname,"views/homePage.html"));
 })
-
-
 app.get('/myCart',(req,res) => {
     res.sendFile(path.join(__dirname,"views/myCart.html"));
 })
@@ -39,91 +38,17 @@ app.get('/products',(req,res) => {
 app.get('/SignIn',(req,res) => {
     res.sendFile(path.join(__dirname,"views/signIn.html"));
 })
-
 app.get('/SignUp',(req,res) => {
     res.sendFile(path.join(__dirname,"views/signUp.html"));
 })
-
 app.get('/SignOut',(req,res) => {
     res.clearCookie("id")
     res.sendFile(path.join(__dirname,"views/signIn.html"));
 })
 
-
-app.post('/UserSignUp',(req,res) => {
-if (!req.body){
-    res.status(400).send({message:"form cant be empty"});
-    return;
-}
-
-
-const newUser = {
-    "id": req.body.id,
-    "email": req.body.email,
-    "psw": req.body.psw,
-    "name": req.body.name,
-    "phone": req.body.phoneNum
-
-}
-const q1 = "insert into users set ?";
-sql.query(q1,newUser,(err,sqlres)=>{
-    if(err){
-        console.log("error in q1:",err);
-        res.status(400).send({message:"New User sign up didn't work, Please try again"});
-        return;
-    }
-    console.log("created new user:", {id:sqlres.insertId});
-    res.cookie ("id",req.body.id);
-    res.cookie ("email",req.body.email);
-    res.cookie ("psw",req.body.psw);
-    res.cookie ("name",req.body.name);
-    res.cookie ("phone",req.body.phoneNum);
-    res.redirect('/SignIn');
-    return;
-});
-});
-
-const showUsers = (req, res) => {
-    const q2 = "SELECT * FROM users";
-    sql.query(q2, (err, sqlres) => {
-        if (err) {
-            console.log("error in q2:", err);
-            res.status(400).send({ message: "Can't show all users" });
-            return;
-        }
-        res.send(sqlres);
-        return;
-    });
-};
-
-app.get('/showUsers',showUsers);
-
-
-app.post('/signInUser', (req, res) => {
-    const { loginEmail, loginPsw } = req.body; // Extract email and password from the request body
-
-    const q3 = "SELECT * FROM users WHERE email = ? AND psw = ?";
-    res.cookie('email', loginEmail);
-    res.cookie('psw', loginPsw);
-    let  a = loginEmail;
-    let b = loginPsw;
-    sql.query(q3, [a, b], (err, sqlres) => {
-        if (err) {
-            console.log("error in q3:", err);
-            res.status(400).send({ message: "Sign-in failed. Please try again." });
-            return;
-        }
-
-        if (sqlres.length > 0) {
-            // User exists, redirect to the homepage
-            res.redirect('/homepage');
-
-        } else {
-            // User does not exist, display an error message
-            res.status(401).send({ message: "not exist: " + sqlres.length });
-        }
-    });
-});
+app.post('/UserSignUp', crud.createNewUser);
+app.get('/showUsers',crud.showUsers);
+app.post('/signInUser',crud.userLogin);
 
 
 app.listen(port, ()=> {
---
+++ express_server/static/crud.js
@@ -0,0 +1,74 @@
+const sql = require("../db/db");
+
+const createNewUser = (req,res) => {
+    if (!req.body){
+        res.status(400).send({message:"form cant be empty"});
+        return;
+    }
+    const newUser = {
+        "id": req.body.id,
+        "email": req.body.email,
+        "psw": req.body.psw,
+        "name": req.body.name,
+        "phone": req.body.phoneNum
+    }
+    const q1 = "insert into users set ?";
+    sql.query(q1,newUser,(err,sqlres)=>{
+        if(err){
+            console.log("error in q1:",err);
+            res.status(400).send({message:"New User sign up didn't work, Please try again"});
+            return;
+        }
+        console.log("created new user:", {id:sqlres.insertId});
+        res.cookie ("id",req.body.id);
+        res.cookie ("email",req.body.email);
+        res.cookie ("psw",req.body.psw);
+        res.cookie ("name",req.body.name);
+        res.cookie ("phone",req.body.phoneNum);
+        res.redirect('/SignIn');
+        return;
+    });
+}
+
+//login to the site
+const userLogin = (req, res) => {
+    const { loginEmail, loginPsw } = req.body; // Extract email and password from the request body
+
+    const q3 = "SELECT * FROM users WHERE email = ? AND psw = ?";
+    res.cookie('email', loginEmail);
+    res.cookie('psw', loginPsw);
+    let  a = loginEmail;
+    let b = loginPsw;
+    sql.query(q3, [a, b], (err, sqlres) => {
+        if (err) {
+            console.log("error in q3:", err);
+            res.status(400).send({ message: "Sign-in failed. Please try again." });
+            return;
+        }
+        if (sqlres.length > 0) {
+            // User exists, redirect to the homepage
+            res.redirect('/homepage');
+        } else {
+            // User does not exist, display an error message
+            res.status(401).send({ message: "not exist: " + sqlres.length });
+        }
+    });
+}
+
+
+const showUsers = (req, res) => {
+    const q2 = "SELECT * FROM users";
+    sql.query(q2, (err, sqlres) => {
+        if (err) {
+            console.log("error in q2:", err);
+            res.status(400).send({ message: "Can't show all users" });
+            return;
+        }
+        res.send(sqlres);
+        return;
+    });
+};
+
+
+
+module.exports = { createNewUser,userLogin, showUsers};
