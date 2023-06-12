f5ff7edf409469551ec9b51fb092e1c2c5c52688
amitf <fedaeli@post.bgu.ac.il>
2023-06-12 13:33:03 +0300

    after server

--- express_server/app.js
+++ express_server/app.js
@@ -25,6 +25,8 @@ app.get('/contactUs',(req,res) => {
 app.get('/homePage',(req,res) => {
     res.sendFile(path.join(__dirname,"views/homePage.html"));
 })
+
+
 app.get('/myCart',(req,res) => {
     res.sendFile(path.join(__dirname,"views/myCart.html"));
 })
@@ -53,12 +55,15 @@ if (!req.body){
     res.status(400).send({message:"form cant be empty"});
     return;
 }
+
+
 const newUser = {
     "id": req.body.id,
     "email": req.body.email,
     "psw": req.body.psw,
     "name": req.body.name,
     "phone": req.body.phoneNum
+
 }
 const q1 = "insert into users set ?";
 sql.query(q1,newUser,(err,sqlres)=>{
@@ -90,7 +95,36 @@ const showUsers = (req, res) => {
         return;
     });
 };
+
 app.get('/showUsers',showUsers);
+
+
+app.post('/signInUser', (req, res) => {
+    const { loginEmail, loginPsw } = req.body; // Extract email and password from the request body
+
+    const q3 = "SELECT * FROM users WHERE email = ? AND psw = ?";
+    res.cookie('email', loginEmail);
+    res.cookie('psw', loginPsw);
+    let  a = loginEmail;
+    let b = loginPsw;
+    sql.query(q3, [a, b], (err, sqlres) => {
+        if (err) {
+            console.log("error in q3:", err);
+            res.status(400).send({ message: "Sign-in failed. Please try again." });
+            return;
+        }
+
+        if (sqlres.length > 0) {
+            // User exists, redirect to the homepage
+            res.redirect('/homepage');
+
+        } else {
+            // User does not exist, display an error message
+            res.status(401).send({ message: "not exist: " + sqlres.length });
+        }
+    });
+});
+
 
 app.listen(port, ()=> {
     console.log("Server is running on port:",port);
