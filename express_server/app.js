3ed3823b7927bb2298c5a47b97080069e560dcc3
Eyal Arkush <eyal@arkush.co.il>
2023-06-13 20:22:27 +0300

    after server

--- express_server/app.js
+++ express_server/app.js
@@ -2,54 +2,108 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = 3000;
-const crud = require("../express_server/static/crud");
-const sql = require('../express_server/db/db');
 const BodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
 app.use(BodyParser.json());
+const cookieParser = require("cookie-parser");
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname,"static")));
 app.use(BodyParser.urlencoded({extended:true}));
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine','pug');
+const sql = require('../express_server/db/db');
+const createDBCRUD = require('./db/CRUDDB');
+
+// app.get('/CRUD',createDBCRUD.createTable);
 
 app.get('/',(req,res) => {
      res.sendFile(path.join(__dirname,"views/signIn.html"));
-    // res.render('index');
 })
 app.get('/aboutUs',(req,res) => {
-    res.sendFile(path.join(__dirname,"views/aboutUs.html"));
+    // res.sendFile(path.join(__dirname,"views/aboutUs.html"));
+    res.render('aboutUs');
 })
+
 app.get('/contactUs',(req,res) => {
-    res.sendFile(path.join(__dirname,"views/contactUs.html"));
+    // res.sendFile(path.join(__dirname,"views/contactUs.html"));
+    res.render('contactUs');
 })
 app.get('/homePage',(req,res) => {
-    res.sendFile(path.join(__dirname,"views/homePage.html"));
+    // res.sendFile(path.join(__dirname,"views/homePage.html"));
+    res.render('homePage');
+
 })
 app.get('/myCart',(req,res) => {
-    res.sendFile(path.join(__dirname,"views/myCart.html"));
+    // res.sendFile(path.join(__dirname,"views/myCart.html"));
+    res.render('myCart');
+
 })
 app.get('/myOrder',(req,res) => {
-    res.sendFile(path.join(__dirname,"views/myOrder.html"));
+    // res.sendFile(path.join(__dirname,"views/myOrder.html"));
+    res.render('myOrder');
 })
+
 app.get('/products',(req,res) => {
-    res.sendFile(path.join(__dirname,"views/products.html"));
+    // res.sendFile(path.join(__dirname,"views/products.html"));
+    res.render('products');
 })
 app.get('/SignIn',(req,res) => {
-    res.sendFile(path.join(__dirname,"views/signIn.html"));
+    // res.sendFile(path.join(__dirname,"views/signIn.html"));
+    res.render('signIn');
 })
+
 app.get('/SignUp',(req,res) => {
-    res.sendFile(path.join(__dirname,"views/signUp.html"));
+    // res.sendFile(path.join(__dirname,"views/signUp.html"));
+    res.render('signUp');
 })
+
 app.get('/SignOut',(req,res) => {
     res.clearCookie("id")
     res.sendFile(path.join(__dirname,"views/signIn.html"));
 })
 
-app.post('/UserSignUp', crud.createNewUser);
-app.get('/showUsers',crud.showUsers);
-app.post('/signInUser',crud.userLogin);
+app.post('/UserSignUp',(req,res) => {
+if (!req.body){
+    res.status(400).send({message:"form cant be empty"});
+    return;
+}
+const newUser = {
+    "id": req.body.id,
+    "email": req.body.email,
+    "psw": req.body.psw,
+    "name": req.body.name,
+    "phone": req.body.phoneNum
+}
+const q1 = "insert into users set ?";
+sql.query(q1,newUser,(err,sqlres)=>{
+    if(err){
+        console.log("error in q1:",err);
+        res.status(400).send({message:"New User sign up didn't work, Please try again"});
+        return;
+    }
+    console.log("created new user:", {id:sqlres.insertId});
+    res.cookie ("id",req.body.id);
+    res.cookie ("email",req.body.email);
+    res.cookie ("psw",req.body.psw);
+    res.cookie ("name",req.body.name);
+    res.cookie ("phone",req.body.phoneNum);
+    res.redirect('/SignIn');
+    return;
+});
+});
 
+const showUsers = (req, res) => {
+    const q2 = "SELECT * FROM users";
+    sql.query(q2, (err, sqlres) => {
+        if (err) {
+            console.log("error in q2:", err);
+            res.status(400).send({ message: "Can't show all users" });
+            return;
+        }
+        res.send(sqlres);
+        return;
+    });
+};
+app.get('/showUsers',showUsers);
 
 app.listen(port, ()=> {
     console.log("Server is running on port:",port);
---
+++ express_server/db/CRUDDB.js
@@ -0,0 +1,9 @@
+// const db = require('../express_server/db/db');
+// const createTable = (req,res) =>{
+//     const q1 = 'CREATE TABLE IF NOT EXISTS table1 (id INT(10) NOT NULL, name VARCHAR(255), email VARCHAR(255))';
+//     db.query(q1, (err, result) => {
+//     sql.query(q1,(err,mysqlres) => {
+//         if (err) throw err;
+//         res.send('Table Created')
+//     })
+// };
