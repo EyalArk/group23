b42f5520b121fdecd66bcb9562fafd9464ea9aa4
Eyal Arkush <eyal@arkush.co.il>
2023-06-18 12:26:17 +0300

    after server!

--- express_server/db/CreateDB_CRUD.js
+++ express_server/db/CreateDB_CRUD.js
@@ -1,6 +1,6 @@
 const sql = require('./db');
 const CreateTable = (req,res)=>{
-  const q3 = 'CREATE TABLE IF NOT EXISTS users (id INT(10) NOT NULL, name VARCHAR(255), email VARCHAR(255))';
+  const q3 = 'CREATE TABLE IF NOT EXISTS users (email varchar(255) NOT NULL, psw int, name VARCHAR(255), phone int(255))';
   sql.query(q3, (err, mysqlres)=>{
       if (err) throw err;
       res.send("Table created:")
@@ -8,7 +8,7 @@ const CreateTable = (req,res)=>{
 };
 
 const DropTable = (req,res)=>{
-    const q4 = 'DROP TABLE table1';
+    const q4 = 'DROP TABLE users';
     sql.query(q4, (err, mysqlres)=>{
         if (err) throw err;
         res.send("Table dropped")
