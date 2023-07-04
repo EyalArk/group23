68535e1afc6b5a0a95befc6779f69a9f0f7e493a
Eyal Arkush <eyal@arkush.co.il>
2023-07-04 14:54:39 +0300

    Revert "after server!"
    This reverts commit e3bc0e1f9c01fa2273588f6cb0e8bb8633f8cedd.

--- express_server/db/CreateDB_CRUD.js
+++ express_server/db/CreateDB_CRUD.js
@@ -22,7 +22,7 @@ const createTables = (req, res, next) => {  // function for create all the table
 
     const q3 = `
     CREATE TABLE IF NOT EXISTS products (
-  productId VARCHAR(255) NOT NULL,
+  productId int NOT NULL,
   name VARCHAR(255),
   fit VARCHAR(255),
   color1 VARCHAR(255),
@@ -39,7 +39,7 @@ const createTables = (req, res, next) => {  // function for create all the table
 CREATE TABLE IF NOT EXISTS cart (
   cartId INT NOT NULL AUTO_INCREMENT,
   userEmail VARCHAR(255),
-  productId VARCHAR(255),
+  productId INT,
   quantity INT DEFAULT 1,
   PRIMARY KEY (cartId),
   FOREIGN KEY (userEmail) REFERENCES users(email),
